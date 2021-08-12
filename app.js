f12a7e42d369341ed2dcdbf7342e1ac661e0fae0
Zack Lyon <zack.j.lyon@gmail.com>
2021-08-12 16:18:37 -0700

    add calculator that does add, subtract, multiply, divide, shows counters for each

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 // import functions and grab DOM elements
-
+import { add, subtract, multiply, divide } from './util.js';
 // initialize state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+const addBtn = document.getElementById('add-btn');
+const subtractBtn = document.getElementById('subtract-btn');
+const multiplyBtn = document.getElementById('multiply-btn');
+const divideBtn = document.getElementById('divide-btn');
+const numOne = document.getElementById('num-one');
+const numTwo = document.getElementById('num-two');
+const output = document.getElementById('output');
+const operator = document.getElementById('operator');
+const addCount = document.getElementById('add-count');
+const subtractCount = document.getElementById('subtract-count');
+const multiplyCount = document.getElementById('multiply-count');
+const divideCount = document.getElementById('divide-count');
+let timesAdded = 0;
+let timesSubtracted = 0;
+let timesMultiplied = 0;
+let timesDivided = 0;
+
+addBtn.addEventListener('click', () => {
+    output.textContent = add(numOne.value, numTwo.value);
+    operator.textContent = '+';
+    timesAdded++;
+    addCount.textContent = timesAdded;
+});
+
+subtractBtn.addEventListener('click', () => {
+    output.textContent = subtract(numOne.value, numTwo.value);
+    operator.textContent = '-';
+    timesSubtracted++;
+    subtractCount.textContent = timesSubtracted;
+});
+
+multiplyBtn.addEventListener('click', () => {
+    output.textContent = multiply(numOne.value, numTwo.value);
+    operator.textContent = '*';
+    timesMultiplied++;
+    multiplyCount.textContent = timesMultiplied;
+});
+
+divideBtn.addEventListener('click', () => {
+    output.textContent = divide(numOne.value, numTwo.value);
+    operator.textContent = '/';
+    timesDivided++;
+    divideCount.textContent = timesDivided;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,32 @@
 import './example.test.js';
+import { add, subtract, multiply, divide } from '../util.js';
+
+const addTest = QUnit.test;
+const subtractTest = QUnit.test;
+const multiplyTest = QUnit.test;
+const divideTest = QUnit.test;
+
+
+addTest ('5 plus 3 = 8', (expectedSum) => {
+    const expectSum = 8;
+    const actualSum = add(5, 3);
+    expectedSum.equal(expectSum, actualSum);
+});
+subtractTest ('5 minus 3 = 2', (expectedDiff) => {
+    const expectDiff = 2;
+    const actualDiff = subtract(5, 3);
+    expectedDiff.equal(expectDiff, actualDiff);
+});
+
+multiplyTest ('5 times 3 = 15', (expectedQuot) => {
+    const expectQuot = 15;
+    const actualQuot = multiply(5, 3);
+    expectedQuot.equal(expectQuot, actualQuot);
+});
+
+divideTest ('15 divided by 3 = 5', (expectedRemain) => {
+    const expectRemain = 5;
+    const actualRemain = divide(15, 3);
+    expectedRemain.equal(expectRemain, actualRemain);
+});
+
